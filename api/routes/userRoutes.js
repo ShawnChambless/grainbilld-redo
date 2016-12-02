f7a81014ee706ebfe7cfda92158426fb11b104e1
ShawnChambless <shawn.chambless@gmail.com>
2016-12-01 20:01:33 -0600

    refactored userRoutes

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -8,12 +8,15 @@
 
 
 	UserRouter
-			.post('/api/users', userCtrl.createUser)
-			.get('/api/users/getUser', userCtrl.getCurrentUser)
 			.get('/api/user/recipes/:userId', userCtrl.getRecipes)
+			.get('/api/users/getUser', userCtrl.getCurrentUser)
+
 			.put('/api/user/recipes/removeUser', recipeCtrl.removeRecipe)
 			.put('/api/users/:user_id', userCtrl.updateUser)
+
+			.post('/api/users', userCtrl.createUser)
 			.post('/api/users/newRecipe', recipeCtrl.newRecipe)
+
 			.delete('/api/users/favorites/remove:id', userCtrl.removeFavorite)
 			.delete('/api/users/:user_id', userCtrl.removeUser);
 
